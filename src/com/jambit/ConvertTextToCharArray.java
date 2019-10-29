901123f83c095d32c7149cb75a81655da4ae3f96
TobiasAnzinger <tobiasanzinger98@gmail.com>
2019-10-29 10:44:10 +0100

    added feature InputReader

--- src/com/jambit/ConvertTextToCharArray.java
+++
@@ -1,5 +0,0 @@
-package com.jambit;
-
-public class ConvertTextToCharArray {
-
-}
--- src/com/jambit/Main.java
+++ src/com/jambit/Main.java
@@ -1,14 +1,14 @@
 package com.jambit;
 
+import com.sun.xml.internal.fastinfoset.util.StringArray;
+
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-//        DecryptionKey enterTheKey = new DecryptionKey();
-//        enterTheKey.enterDecryptionKey();
 
-        ReadTheEncryptedFile inputFile = new ReadTheEncryptedFile();
-        System.out.println(inputFile.readThisLine(3));
+        InputReader inputReader = new InputReader();
+        final StringArray fileContent = inputReader.readFile("EncryptedText.txt");
     }
 }
