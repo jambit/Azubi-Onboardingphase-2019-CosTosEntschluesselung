d8dfd9120118c3963ac0b2b38ae57a46c6c821ee
TobiasAnzinger <tobiasanzinger98@gmail.com>
2019-10-30 09:19:14 +0100

    feature decryptionHelper added key and seed decryption added feature UserInterface

--- src/com/jambit/Main.java
+++ src/com/jambit/Main.java
@@ -9,24 +9,8 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        InputReader inputReader = new InputReader();
-        final StringArray fileContent = inputReader.readFile("EncryptedText.txt");
-
         UserInterface userInterface = new UserInterface();
         userInterface.enterKey();
 
-        DecryptionHelper decryptionHelper = new DecryptionHelper();
-        StringArray decryptFileContent;
-        if (userInterface.seed != 0) {
-            decryptFileContent = decryptionHelper.decrypt(fileContent, userInterface.key,
-                    userInterface.seed);
-        } else {
-            decryptFileContent = decryptionHelper.decrypt(fileContent, userInterface.key);
-        }
-
-        // prints out the decrypted text
-        for (int i = 0; i < decryptFileContent.getSize(); i++) {
-            System.out.println(decryptFileContent.get(i));
-        }
     }
 }
